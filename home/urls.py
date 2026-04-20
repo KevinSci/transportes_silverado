81d618bf478ede88255cf39f0245c5cd9578c54a
kevlp <kevlp@outlook.es>
2026-04-20 14:15:19 -0600

    Implementar control de acceso para vistas de administración y mejorar estilos de botones

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
+from django.contrib.auth.decorators import user_passes_test
 from . import views
 
+def is_admin_user(user):
+    return user.is_authenticated and user.role in ['administrador', 'propietario']
+
 urlpatterns = [
-    path('', views.index_admin, name='index_admin'),
-    path('users/', views.show_users, name='users'),
-    path('users/create_user/', views.create_user, name='create_user'),
-    path('users/delete/<int:user_id>/', views.delete_users, name='delete_user'),
-    path('maintenance/', views.maintenance, name='maintenance'),
-    path('service/', views.service, name='service'),
-    path('plots/', views.plots, name='plots'),
+    path('', user_passes_test(is_admin_user)(views.index_admin), name='index_admin'),
+    path('users/', user_passes_test(is_admin_user)(views.show_users), name='users'),
+    path('users/create_user/', user_passes_test(is_admin_user)(views.create_user), name='create_user'),
+    path('users/delete/<int:user_id>/', user_passes_test(is_admin_user)(views.delete_users), name='delete_user'),
+    path('maintenance/', user_passes_test(is_admin_user)(views.maintenance), name='maintenance'),
+    path('service/', user_passes_test(is_admin_user)(views.service), name='service'),
+    path('plots/', user_passes_test(is_admin_user)(views.plots), name='plots'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 # home/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpRequest
 from django.contrib import messages
 from django.core.paginator import Paginator
@@ -8,16 +7,12 @@ from .controllers import users
 
 
 # Solo los admins pueden acceder a las vistas
-def is_admin_user(user):
-    return user.is_authenticated and user.role in ['administrador', 'propietario']
 
-@user_passes_test(is_admin_user, login_url='login')
 def index_admin(request):
     return render(request, 'index.html')
 
 # =========== Usuarios =========== #
 
-@user_passes_test(is_admin_user, login_url='login')
 def show_users(request: HttpRequest):
     search_query = request.GET.get('q', '')
     page_number = request.GET.get('page', 1)
@@ -26,7 +21,6 @@ def show_users(request: HttpRequest):
     page_obj = paginator.get_page(page_number)
     return render(request, 'users/users.html', {'page_obj': page_obj, 'search_query': search_query})
 
-@user_passes_test(is_admin_user, login_url='login')
 def create_user(request: HttpRequest):
     if request.method == 'POST':
         try:
@@ -36,7 +30,6 @@ def create_user(request: HttpRequest):
         except Exception as e:
             messages.error(request, e.message)
     return render(request, 'users/create_user.html')
-
 
 def delete_users(request: HttpRequest, user_id: int):
     if request.method == 'POST':
@@ -50,14 +43,15 @@ def delete_users(request: HttpRequest, user_id: int):
 def edit_users(request: HttpRequest):
     pass
 
-@user_passes_test(is_admin_user, login_url='login')
+# =========== Activos =========== #
+
 def maintenance(request):
     return render(request, 'maintenance.html')
 
-@user_passes_test(is_admin_user, login_url='login')
+
 def service(request):
     return render(request, 'service.html')
 
-@user_passes_test(is_admin_user, login_url='login')
+
 def plots(request):
     return render(request, 'plots.html')
